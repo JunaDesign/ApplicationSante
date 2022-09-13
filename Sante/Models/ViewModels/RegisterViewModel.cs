ea14aa92f0672a73d1b5bcf429c3c48c17d89a33
Beau <ambeauda@gmail.com>
2022-09-13 17:14:12 -0400

    -Add Code to the AccountController
    - Add code to the Register
    
    -Add to LoginPartial.

--- Sante/Models/ViewModels/RegisterViewModel.cs
+++ Sante/Models/ViewModels/RegisterViewModel.cs
@@ -14,7 +14,7 @@ namespace Sante.Models.ViewModels
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
--- Sante/Startup.cs
+++ Sante/Startup.cs
@@ -49,7 +49,7 @@ namespace Sante
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
